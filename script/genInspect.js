4a54ccd5e40046fc15ec54053283091ed21a1523
Tyler Young <youngtyj@hotmail.com>
2015-01-14 12:35:08 -0500

    Added a for loop on line 76 in order to loop through the checkbox values as they were not being called back from localStorage.

--- script/genInspect.js
+++ script/genInspect.js
@@ -19,10 +19,33 @@ function init(){
 		//console.log(checkBoxesArr);
 
 		//counter is initialized for array walkthrough
-		var loopThrough = 0;
+		//var loopThrough = 0;
 		
+		//new counter initialized to create array
+		var val = [];
+		val[0] = window.localStorage.getItem('check0');
+		val[1] = window.localStorage.getItem('check1');
+		val[2] = window.localStorage.getItem('check2');
+		val[3] = window.localStorage.getItem('check3');
+		val[4] = window.localStorage.getItem('check4');
+		val[5] = window.localStorage.getItem('check5');
+		val[6] = window.localStorage.getItem('check6');
+		val[7] = window.localStorage.getItem('check7');
+		val[8] = window.localStorage.getItem('check8');
+		val[9] = window.localStorage.getItem('check9');
+		val[10] = window.localStorage.getItem('check10');
+		val[11] = window.localStorage.getItem('check11');
+		val[12] = window.localStorage.getItem('check12');
+		val[13] = window.localStorage.getItem('check13');
+		val[14] = window.localStorage.getItem('check14');
+		val[15] = window.localStorage.getItem('check15');
+		val[16] = window.localStorage.getItem('check16');
+		val[17] = window.localStorage.getItem('check17');
+		val[18] = window.localStorage.getItem('check18');
+		val[19] = window.localStorage.getItem('check19');
 		//variable is set for each possible space in localmemory
 		//THIS NEEDS TO BE MORE DYNAMIC
+		/*
 		var val0 = window.localStorage.getItem('check0');
 		var val1 = window.localStorage.getItem('check1');
 		var val2 = window.localStorage.getItem('check2');
@@ -42,7 +65,7 @@ function init(){
 		var val16 = window.localStorage.getItem('check16');
 		var val17 = window.localStorage.getItem('check17');
 		var val18 = window.localStorage.getItem('check18');
-		var val19 = window.localStorage.getItem('check19');
+		var val19 = window.localStorage.getItem('check19'); */
 
 		
 		//the values from localstorage are cycled through and compared to the values
@@ -50,14 +73,24 @@ function init(){
 		//if a match is found the field is populated or the checkbox is checked
 		//THIS ALSO NEEDS TO BE MORE DYNAMIC
 		$.each(checkBoxesArr, function(){
-			var valueSet = checkBoxesArr[loopThrough];
+			for(b = 0; b < checkBoxesArr.length; b++){
+				var valueSet = checkBoxesArr[b];
+			}
+			
 			
 
-			console.log(valueSet);
+			// console.log(valueSet);
 			
 			//Values are cycled through
 			//NEEDS TO BE DYNAMIC CHANGE TO FOR STATEMENT
-			if(val0 == valueSet){
+			for (i = 0; i < val.length; i++) { 
+    				if(val[i] == valueSet){
+				
+					$("input[value='" + val[i] + "']").prop('checked', true);
+				
+				}
+			}
+			/* if(val0 == valueSet){
 				
 				$("input[value='" + val0 + "']").prop('checked', true);
 				
@@ -158,7 +191,7 @@ function init(){
 				
 			}
 
-			loopThrough++;
+			loopThrough++; */
 		});		
 
 	}
